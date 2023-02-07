cad264ef5393d48c9282d06fb90365d7ac8cb1fd
unknown <laurynas.gipas@gmail.com>
2023-02-07 13:48:36 +0200

    attendeescontext added to logout

--- frontend/src/components/hooks/useLogout.js
+++ frontend/src/components/hooks/useLogout.js
@@ -1,11 +1,14 @@
 import { useAuthContext } from "./useAuthContext"
+import { useAttendeesContext } from "./useAttendeesContext";
 
 export const useLogout = () => {
-    const {dispatch } = useAuthContext();
+    const { dispatch } = useAuthContext();
+    const { dispatch: attendeesDispatch } = useAttendeesContext();
 
     const logout = () => {
         localStorage.removeItem('user')
         dispatch({type: 'LOGOUT'})
+        attendeesDispatch({type: 'SET_ATTENDEES', payload: null})
     }
     return {logout}
 }
